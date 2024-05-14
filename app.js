4a6931a529fbaaebb8c9f426efb904cc9253bf12
tvbacs <162428651+tvbacs@users.noreply.github.com>
2024-05-14 14:27:14 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Create connection to MySQL database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'boc'
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the database');
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query('SELECT * FROM user WHERE username = ? AND password = ?', [username, password], (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        if (result.length > 0) {
+            res.send("OK");
+        } else {
+            res.send("KO");
+        }
+    });
+});
+
+app.post('/user', (req, res) => {
+    console.log("hqwdshu2qewxcdhnewxdnj");
+    res.send('User route accessed');
+});
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
