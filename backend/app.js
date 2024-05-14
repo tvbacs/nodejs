907509d2655e72297ba488cb0b0a96074c64e763
quang <qhoang1050@gmail.com>
2024-05-14 21:45:28 +0700

    ok

--- backend/app.js
+++ backend/app.js
@@ -42,6 +42,6 @@ app.post('/user', (req, res) => {
     res.send('User route accessed');
 });
 
-app.listen(5000, () => {
+app.listen(4000, () => {
     console.log('Server is running on port 5000');
 });
