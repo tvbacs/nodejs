30fd0b1b87e6944505886efbed5fd067980a2f3d
quang <qhoang1050@gmail.com>
2024-05-16 09:06:19 +0700

    ok133

--- api/static_file.js
+++ api/static_file.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
 // Serve static files from the "public" and "assets" directories
-app.use(express.static('./public'));
-app.use(express.static('./assets'));
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../assets')));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
